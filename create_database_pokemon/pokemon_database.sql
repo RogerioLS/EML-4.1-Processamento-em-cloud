69b49ec0f4e3b899758f9dde8a5688c1b3abbcfc
RogerioLS <rogerio_288@hotmail.com>
2024-07-31 23:43:47 -0300

    create data base pokemon

---
+++ create_database_pokemon/pokemon_database.sql
@@ -0,0 +1,11 @@
+create database pokemon_database;
+
+create table pokemon_database.pokemon(
+	passenger_id int primary key,
+	id_pokemon varchar(255),
+	name varchar(255),
+	types varchar(255),
+	height varchar(255),
+	weight varchar(255),
+	moves varchar(255)
+);
